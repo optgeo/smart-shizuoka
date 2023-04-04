eb2f490e70273569e4ae6620ab3a53496da65a7d
FUJIMURA Hidenori <hfu@users.noreply.github.com>
2023-04-04 21:58:45 +0900

    update

--- once/list.rb
+++ once/list.rb
@@ -3,6 +3,6 @@ print <<-EOS
 EOS
 `ruby once/tilesets.rb | grep point-cloud`.split("\n").each {|l|
   print <<-EOS
-1. [#{l.split('/')[-3..-2].join('/')}](https://optgeo.github.io/smart-shizuoka/?#{l})
+1. [#{l.split('/')[-3..-2].join('/')}](https://optgeo.github.io/smart-shizuoka/?tileset=#{l})
   EOS
 }
